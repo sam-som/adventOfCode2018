f6af37a7f4db294b9356bf62d13a1e8cddbb7c1e
Sam Som <sams@cacpqua-samsom.local>
2018-12-08 16:19:40 -0800

    Complete 1 day of Avent of Code 2018

---
+++ day_1_puzzle.rb
@@ -0,0 +1,29 @@
+class Day1Puzzle
+    def frequencyFinder()
+        currentFrequency = 0
+        File.open("input/day_1_puzzle_1.txt").each do |line|
+            currentFrequency += line.to_i
+        end
+        puts currentFrequency
+    end
+
+    def repeatFinder()
+        knownFrequencies = [0]
+        currentFrequency = 0
+        while true
+            File.open("input/day_1_puzzle_1.txt").each do |line|
+                currentFrequency += line.to_i
+                if(knownFrequencies.include? currentFrequency)
+                    puts "First repeat frequency is " + currentFrequency.to_s
+                    return
+                else
+                    knownFrequencies << currentFrequency
+                end
+            end
+        end
+    end
+end
+
+#create Day1Puzzle object
+# Day1Puzzle.new.frequencyFinder
+# Day1Puzzle.new.repeatFinder()
